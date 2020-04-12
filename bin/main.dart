efdd92e756acc7e76d33012e5d1a2300af563c5b
Michel Gomes da Silva <michel@uconex.com.br>
2020-04-12 21:06:47 +0100

    arguments test

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,10 @@
 import 'package:estudo/estudo.dart' as estudo;
 
 void main(List<String> arguments) {
+  if (arguments[0] == 'hackear'){
+    print('hackeamento concluido');
+  }else if (arguments[0] == 'imprimir'){
+    print('executando impressão');
+  }
   print('Hello world: ${estudo.calculate()}!');
 }
